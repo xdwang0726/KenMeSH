834478b51855c456e667cd8e6998531de4f5c9e7
xdwang0726 <starwxd@gmail.com>
2019-10-10 21:49:45 -0400

    extract data from original json file

---
+++ get_data.py
@@ -0,0 +1,55 @@
+import ijson
+from tqdm import tqdm
+
+""" get text(abstract and title) and MeSH terms to each document """
+f = open('/Users/wangxindi/Downloads/allMeSH_2019.json', encoding="utf8")
+
+objects = ijson.items(f, 'articles.item')
+
+ids_list = []
+text = []
+labels = []
+
+for obj in tqdm(objects):
+    try:
+        ids = obj["pmid"].strip()
+        text = obj["title"].strip() + " " + obj["abstractText"].strip()
+        label = obj["meshMajor"]
+        ids_list.append(ids)
+        text.append(text)
+        labels.append(label)
+    except AttributeError:
+        print(obj["pmid"].strip())
+
+""" mapping mesh terms to meshIDs """
+mapping_id = {}
+with open('MeSH_name_id_mapping_2019.txt') as f:
+    for line in f:
+        (key, value) = line.split('=')
+        mapping_id[key] = value
+
+mesh_id_list = []
+for mesh in labels:
+    new_mesh = []
+    for item in mesh:
+        index = mapping_id.get(item.strip())
+        new_mesh.append(index.strip())
+    mesh_id_list.append(new_mesh)
+
+file = open("train_meshID.txt", "w", encoding='utf-8')
+for meshID in mesh_id_list:
+    allID = '|'.join(meshID)
+    file.write(allID.strip() + "\r")
+file.close()
+
+file = open("train_text.txt", "w", encoding='utf-8')
+for i, txt in enumerate(text):
+    document = ids_list[i] + "|" + txt
+    file.write(document.strip() + "\r")
+file.close()
+
+file = open("train_meshList.txt", "w", encoding='utf-8')
+for i, mesh in enumerate(labels):
+    m = ids_list[i] + '||' + '|'.join(mesh)
+    file.write(m.strip() + "\r")
+file.close()
