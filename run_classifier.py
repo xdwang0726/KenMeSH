485e560acfc7ede751ee83b6169253bdebc2ca4d
xdwang0726 <starwxd@gmail.com>
2020-06-29 01:31:00 -0400

    Preliminary codes for MeSH indexing

--- run_classifier.py
+++ run_classifier.py
@@ -32,7 +32,7 @@ def prepare_dataset(train_data_path, test_data_path, mesh_id_list_path, word2vec
 
     print("Loading training data")
     for i, obj in enumerate(tqdm(objects)):
-        if i <= 100000:
+        if i <= 1000:
             try:
                 ids = obj["pmid"].strip()
                 text = obj["abstractText"].strip()
