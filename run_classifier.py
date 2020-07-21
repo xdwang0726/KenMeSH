2455c75a5e3bcd7c3719eb3cef9b6136d39566dc
xdwang0726 <starwxd@gmail.com>
2020-07-21 01:02:39 -0400

    baseline

--- run_classifier.py
+++ run_classifier.py
@@ -243,7 +243,7 @@ def main():
     model = MeSH_GCN(vocab_size, args.nKernel, args.ksz, args.hidden_gcn_size, args.embedding_dim)
 
     # model.cnn.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors))
-    model.cnn.embedding_layer.weight.data.copy_(weight_matrix)
+    model.cnn.embedding_layer.weight.data.copy_(torch.from_numpy(weight_matrix))
 
     model.to(device)
     G.to(device)
