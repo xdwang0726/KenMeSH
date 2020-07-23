61bfac3a69d39aeba58fa143807027399133fd73
xdwang0726 <starwxd@gmail.com>
2020-07-23 16:42:10 -0400

    5000000 baseline

--- run_classifier.py
+++ run_classifier.py
@@ -33,7 +33,7 @@ def prepare_dataset(train_data_path, test_data_path, MeSH_id_pair_file, word2vec
     print('Start loading training data')
     logging.info("Start loading training data")
     for i, obj in enumerate(tqdm(objects)):
-        if i <= 4000000:
+        if i <= 5000000:
             try:
                 ids = obj["pmid"]
                 text = obj["abstractText"].strip()
