648c2390a9cdc333188528a4a90c8bd8e9971c39
xdwang0726 <starwxd@gmail.com>
2020-08-07 18:05:41 -0400

    verified train input label

--- run_classifier.py
+++ run_classifier.py
@@ -163,11 +163,11 @@ def train(train_dataset, model, mlb, G, batch_sz, num_epochs, criterion, device,
             optimizer.zero_grad()
             print('3')
             # print('train_original', i, label, '\n')
-            print('4')
             # test_label = mlb.fit_transform(label)
             label = torch.from_numpy(mlb.fit_transform(label)).type(torch.float)
             text, label = text.to(device), label.to(device)
             output = model(text, G, G.ndata['feat'])
+            print('4')
 
             # print train output
             # pred = output.data.cpu().numpy()
@@ -175,6 +175,7 @@ def train(train_dataset, model, mlb, G, batch_sz, num_epochs, criterion, device,
             # top_10_mesh = mlb.inverse_transform(top_10_pred)
             # print('predicted train', i, top_10_mesh, '\n')
 
+            print('5')
             loss = criterion(output, label)
             loss.backward()
             optimizer.step()
@@ -184,8 +185,11 @@ def train(train_dataset, model, mlb, G, batch_sz, num_epochs, criterion, device,
                 sys.stderr.write(
                     "\rProgress: {:3.0f}% lr: {:3.8f} loss: {:3.8f}".format(
                         progress * 100, lr_scheduler.get_last_lr()[0], loss))
+            print('6')
         # Adjust the learning rate
+        print('7')
         lr_scheduler.step()
+        print('8')
 
 
 def test(test_dataset, model, G, batch_sz, device, mlb):
