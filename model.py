32b2b89a31755c018c665d2342f8ab4ea9fe4642
xdwang0726 <starwxd@gmail.com>
2020-08-10 00:35:02 -0400

    debug

--- model.py
+++ model.py
@@ -158,7 +158,7 @@ class MeSH_GCN(nn.Module):
         # label-wise attention (mapping different parts of the document representation to different labels)
         print('w', self.transform.weight.shape)
         print('b', self.transform.bias.shape)
-        x_doc = [torch.tanh(self.transform.weight.matmul(line.transpose(1, 2))) for line in
+        x_doc = [torch.tanh(torch.matmul(line.transpose(1, 2), self.transform.weight.transpose(0, 1))) for line in
                  x_conv]  # [bs, (n_words-ks+1), embedding_sz]
         print("x", x_doc[0].shape, x_doc[1].shape, x_doc[2].shape)
 
