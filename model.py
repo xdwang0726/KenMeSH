73ae571a72781b4e74a791960eda1fa874241f9a
xdwang0726 <starwxd@gmail.com>
2020-08-11 16:46:38 -0400

    debug

--- model.py
+++ model.py
@@ -188,10 +188,10 @@ class MeSH_GCN(nn.Module):
             for i in range(m1.shape[1]):
                 v1 = m1[:, i, :]
                 v2 = m2[:, i]
-                v = torch.matmul(v1, v2).unsqueeze(1)
+                v = torch.matmul(v1, v2).unsqueeze(1).to('cpu')
                 print('v', v.device)
                 print('result', result.device)
-                result = torch.cat((result, v), dim=1).to('cpu')
+                result = torch.cat((result, v), dim=1)
                 #print('result', result.device)
             result.to('cuda')
             return result
