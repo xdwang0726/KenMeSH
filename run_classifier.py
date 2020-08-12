95dc7e16047e37ff44eb27a0618fbe627436de1c
xdwang0726 <starwxd@gmail.com>
2020-08-11 22:22:26 -0400

    debug

--- run_classifier.py
+++ run_classifier.py
@@ -275,7 +275,7 @@ def main():
     parser.add_argument('--embedding_dim', type=int, default=200)
 
     parser.add_argument('--num_epochs', type=int, default=3)
-    parser.add_argument('--batch_sz', type=int, default=2)
+    parser.add_argument('--batch_sz', type=int, default=4)
     parser.add_argument('--num_workers', type=int, default=1)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--weight_decay', type=float, default=0)
