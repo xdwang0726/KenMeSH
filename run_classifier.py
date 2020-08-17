ce06d93fc1757da9e2a742b66581e65fe5e704dc
xdwang0726 <starwxd@gmail.com>
2020-08-17 11:27:09 +0800

    20w example

--- run_classifier.py
+++ run_classifier.py
@@ -298,7 +298,7 @@ def main():
     model = MeSH_GCN(vocab_size, args.nKernel, args.ksz, args.hidden_gcn_size, args.embedding_dim)
 
     # model.cnn.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors))
-    model.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors))
+    # model.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors))
 
     model.to(device)
     G.to(device)
