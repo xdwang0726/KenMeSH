826ed496f2eda13af0d638ba0eb7423e3d6df31f
xdwang0726 <starwxd@gmail.com>
2020-08-19 08:56:41 +0800

    20w example

--- run_classifier.py
+++ run_classifier.py
@@ -165,7 +165,8 @@ def train(train_dataset, model, mlb, G, batch_sz, num_epochs, criterion, device,
             test_label = mlb.fit_transform(label)
             label = torch.from_numpy(mlb.fit_transform(label)).type(torch.float)
             text, label = text.to(device), label.to(device)
-            output = model(text, G, G.ndata['feat'])
+            # output = model(text, G, G.ndata['feat'])
+            output = model(text)
             print('4')
 
             # print train output
