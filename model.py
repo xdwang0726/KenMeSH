a13b2565a96317c7a5746f730a77bb9870a9d08c
xdwang0726 <starwxd@gmail.com>
2020-08-29 16:38:35 +0800

    rgcn debug

--- model.py
+++ model.py
@@ -265,7 +265,7 @@ class RGCNLayer(nn.Module):
                 # an embedding lookup using source node id
                 embed = weight.view(-1, self.out_feat)
                 edges.src['id'].to('cuda')
-                print(edges.src['id'].device)
+                print(edges.data['rel_type'].device, edges.src['id'].device)
                 index = edges.data['rel_type'] * self.in_feat + edges.src['id']
                 return {'msg': embed[index] * edges.data['norm']}
         else:
