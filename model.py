f60ca2a694c831dc622862c1854e48f2813eb73d
xdwang0726 <starwxd@gmail.com>
2020-08-29 16:31:57 +0800

    add rgcn

--- model.py
+++ model.py
@@ -264,8 +264,7 @@ class RGCNLayer(nn.Module):
                 # for input layer, matrix multiply can be converted to be
                 # an embedding lookup using source node id
                 embed = weight.view(-1, self.out_feat)
-                print('device', edges.data['rel_type'].device, edges.src['id'])
-                edges = edges.to('cuda')
+                print('device', edges.data['rel_type'].device, edges.src['id'].device)
                 index = edges.data['rel_type'] * self.in_feat + edges.src['id']
                 return {'msg': embed[index] * edges.data['norm']}
         else:
