1f932aa15ca5860c976a7cac671169890778bdf3
xdwang0726 <starwxd@gmail.com>
2020-09-24 09:07:04 +0800

    lr change to 0.0005

--- run_classifier_gcn.py
+++ run_classifier_gcn.py
@@ -273,7 +273,7 @@ def main():
     parser.add_argument('--num_epochs', type=int, default=3)
     parser.add_argument('--batch_sz', type=int, default=4)
     parser.add_argument('--num_workers', type=int, default=1)
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=5e-4)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=0)
     parser.add_argument('--scheduler_step_sz', type=int, default=5)
