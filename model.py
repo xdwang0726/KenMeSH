f37f96f2c265e8cbab5c8af056b95527c9efff05
xdwang0726 <starwxd@gmail.com>
2020-11-09 17:35:02 +0800

    test rgcn

--- model.py
+++ model.py
@@ -394,7 +394,7 @@ class MeSH_RGCN(nn.Module):
 
         self.content_feature = attenCNN(vocab_size, nKernel, ksz, embedding_dim)
 
-        self.rgcn = EntityClassify(embedding_dim, hidden_rgcn_size, embedding_dim)
+        self.rgcn = EntityClassify(embedding_dim, hidden_rgcn_size, embedding_dim * 2)
 
     def forward(self, input_seq, g, g_node_feature, edge_type, edge_norm):
         x_feature = self.content_feature(input_seq, g_node_feature)
