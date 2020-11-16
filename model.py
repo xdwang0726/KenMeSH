939e64c2218dc90639e7a429fb8df4a9f3e72c87
xdwang0726 <starwxd@gmail.com>
2020-11-16 21:38:13 +0800

    concat gcn/rgcn feature together with original node feature

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ class attenCNN(nn.Module):
         # else:
         #     self.content_final = nn.Linear(len(self.ksz) * self.nKernel, embedding_dim)
 
-        self.content_final = nn.Linear(len(self.ksz) * self.nKernel, embedding_dim)
+        self.content_final = nn.Linear(len(self.ksz) * self.nKernel, embedding_dim * 2)
 
         nn.init.xavier_normal_(self.content_final.weight)
         nn.init.zeros_(self.content_final.bias)
@@ -94,6 +94,7 @@ class attenCNN(nn.Module):
     def forward(self, input_seq, g_node_feat):
         embedded_seq = self.embedding_layer(input_seq)  # size: (bs, seq_len, embed_dim)
         embedded_seq = embedded_seq.unsqueeze(1)
+        embedded_seq = self.dropout(embedded_seq)
 
         x_conv = [F.relu(conv(embedded_seq)).squeeze(3) for conv in self.convs]  # len(Ks) * (bs, kernel_sz, seq_len)
         # label-wise attention (mapping different parts of the document representation to different labels)
@@ -104,7 +105,6 @@ class attenCNN(nn.Module):
         x_content = [torch.matmul(x_conv[i], att) for i, att in enumerate(atten)]
         x_concat = torch.cat(x_content, dim=1)
 
-        x_concat = self.dropout(x_concat)
         x_feature = nn.functional.relu(self.content_final(x_concat.transpose(1, 2)))
 
         return x_feature
@@ -374,7 +374,7 @@ class CorRGCN(nn.Module):
         x_feature = self.content_feature(input_seq, g_node_feature)
 
         label_feature = self.rgcn(g, g_node_feature, edge_type, edge_norm)
-        # label_feature = torch.cat((label_feature, g_node_feature), dim=1)  # torch.Size([29368, 400])
+        label_feature = torch.cat((label_feature, g_node_feature), dim=1)  # torch.Size([29368, 400])
         # if self.add_original_embedding:
         #     label_feature = torch.cat((label_feature, g_node_feature), dim=1)  # torch.Size([29368, 400])
 
