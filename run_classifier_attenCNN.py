8c8bf4c4411e5caea2b2d5aec8c5ffdf977d4f95
xdwang0726 <starwxd@gmail.com>
2021-03-04 21:21:52 +0800

    add dilated cnn

--- run_classifier_attenCNN.py
+++ run_classifier_attenCNN.py
@@ -250,7 +250,7 @@ def main():
 
     parser.add_argument('--device', default='cuda', type=str)
     parser.add_argument('--nKernel', type=int, default=200)
-    parser.add_argument('--ksz', default=3)
+    parser.add_argument('--ksz', default=[3, 4, 5])
     parser.add_argument('--hidden_gcn_size', type=int, default=200)
     parser.add_argument('--embedding_dim', type=int, default=200)
     parser.add_argument('--add_original_embedding', type=bool, default=True)
