2852e6ed3c5496649378234c1ade8c00b1702504
xdwang0726 <starwxd@gmail.com>
2021-03-08 23:56:30 +0800

    add dilated cnn

--- run_classifier_gcn.py
+++ run_classifier_gcn.py
@@ -149,9 +149,8 @@ def generate_batch(batch):
 
 
 def train(train_dataset, model, mlb, G, batch_sz, num_epochs, criterion, device, num_workers, optimizer, lr_scheduler):
-    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-    train_data = DataLoader(train_dataset, batch_size=batch_sz, collate_fn=generate_batch,
-                            num_workers=num_workers, sampler=train_sampler)
+    train_data = DataLoader(train_dataset, batch_size=batch_sz, shuffle=True, collate_fn=generate_batch,
+                            num_workers=num_workers)
 
     num_lines = num_epochs * len(train_data)
 
