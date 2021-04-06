9cd68816a98b073bec5e9cdb95d527414f1cedb4
xdwang0726 <starwxd@gmail.com>
2021-04-06 22:49:21 +0800

    add lstm_dilated cnn

--- run_classifier_gcn.py
+++ run_classifier_gcn.py
@@ -236,7 +236,7 @@ def main():
 
     parser.add_argument('--nKernel', type=int, default=200)
     parser.add_argument('--ksz', default=3)
-    parser.add_argument('--hidden_gcn_size', type=int, default=768)
+    parser.add_argument('--hidden_gcn_size', type=int, default=200)
     parser.add_argument('--embedding_dim', type=int, default=200)
     parser.add_argument('--add_original_embedding', type=bool, default=True)
     parser.add_argument('--dropout', type=float, default=0.2)
