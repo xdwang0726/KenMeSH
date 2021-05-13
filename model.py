a430ad0870cf991348bade0046c80d2f2b935a28
xdwang0726 <starwxd@gmail.com>
2021-05-13 21:01:06 +0800

    DEBUG add title info to multichannel dilated cnn

--- model.py
+++ model.py
@@ -361,9 +361,9 @@ class multichannel_dilatedCNN(nn.Module):
 
         abstract_conv = self.dconv(abstract)  # (bs, embed_dim*2, seq_len-ksz+1)
         # print('dconv', abstract_conv.shape)
-        title_conv = F.relu(self.conv(title)).squeeze(3) # (bs, seq_len-ksz+1, embedding_sz*2)
+        title_conv = F.relu(self.conv(title)) # (bs, seq_len-ksz+1, embedding_sz*2)
         print('title_cov', title_conv.shape)
-        title_conv = title_conv.permute(0, 2, 1)
+        title_conv = title_conv.squeeze(3).permute(0, 2, 1)
 
         # get label features
         label_feature = self.gcn(g, g_node_feature)
