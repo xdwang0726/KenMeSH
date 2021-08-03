9f520a84dd562585cb85d1914426299c5717a39a
xdwang0726 <starwxd@gmail.com>
2021-08-03 18:53:50 +0800

    debug

--- journal_info.py
+++ journal_info.py
@@ -30,7 +30,7 @@ def journal_stats(data_path):
         journal_info = {'counts': 0, 'mesh_counts': {}}
         if journal in journal_dict:
             journal_info['counts'] = journal_info['counts'] + 1
-            mesh_counts[journal] = [mesh_counts['journal'], label_id[i]]
+            mesh_counts[journal] = [mesh_counts[journal], label_id[i]]
         else:
             journal_info['counts'] = 1
             mesh_counts[journal] = [label_id[i]]
