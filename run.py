d3a4a1e4006135e58a0230a4f1e0549e83229fe2
xdwang0726 <starwxd@gmail.com>
2021-09-02 20:33:18 +0800

    debug distributed training

--- run.py
+++ run.py
@@ -99,7 +99,7 @@ def prepare_dataset(train_data_path, test_data_path, MeSH_id_pair_file, word2vec
     test_mesh_mask = []
 
     for i, obj in enumerate(tqdm(test_objects)):
-        if 130000 < i <= 140000:
+        if 1300 < i <= 1400:
             ids = obj['pmid']
             heading = obj['title'].strip()
             text = obj['abstractText'].strip()
@@ -112,7 +112,7 @@ def prepare_dataset(train_data_path, test_data_path, MeSH_id_pair_file, word2vec
             test_text.append(text)
             test_label_id.append(mesh_id)
             test_mesh_mask.append(mesh)
-        elif i > 140000:
+        elif i > 1400:
             break
     print('number of test data %d' % len(test_title))
 
