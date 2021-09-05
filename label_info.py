3816dac234c1d9d6efb0b18273e7f2250f24b53d
xdwang0726 <starwxd@gmail.com>
2021-09-05 21:13:34 +0800

    count mesh in train

--- label_info.py
+++ label_info.py
@@ -53,16 +53,68 @@ def label_count(train_data_path, MeSH_id_pair_file):
     return save_data
 
 
+def new_label_mapping(train_data_path, MeSH_id_pair_file, new_mesh_id_path):
+    # get MeSH in each example
+    f = open(train_data_path, encoding="utf8")
+    objects = ijson.items(f, 'articles.item')
+
+    label_id = []
+
+    print('Start loading training data')
+    for i, obj in enumerate(tqdm(objects)):
+        try:
+            mesh_id = obj['meshId']
+            label_id.append(mesh_id)
+        except AttributeError:
+            print(obj["pmid"].strip())
+
+    flat_label  = list(set([m for meshs in label_id for m in meshs]))
+    # get descriptor and MeSH mapped
+    new_mapping = []
+    with open(MeSH_id_pair_file, 'r') as f:
+        for line in f:
+            (key, value) = line.split('=')
+            if value in flat_label:
+                new_mapping.append(line)
+
+    # count number of nodes and get parent and children edges
+    print('count number of nodes and get edges of the graph %s'.format(len(new_mapping)))
+    with open(new_mesh_id_path, 'w') as f:
+        for item in new_mapping:
+            f.write("%s\n" % item)
+
+    class_freq = {}
+    for doc in label_id:
+        for label in doc:
+            if label in class_freq:
+                class_freq[label] = class_freq[label] + 1
+            else:
+                class_freq[label] = 1
+
+    train_labels = list(class_freq.keys())
+    all_meshIDs = list(new_mapping.values())
+
+    missing_mesh = list(set(all_meshIDs) - set(train_labels))
+    print('missing_mesh', missing_mesh)
+
+    neg_class_freq = {k: len(label_id) - v for k, v in class_freq.items()}
+    save_data = dict(class_freq=class_freq, neg_class_freq=neg_class_freq)
+
+    return save_data
+
+
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--train')
     parser.add_argument('--meSH_pair_path')
+    parser.add_argument('--new_meSH_pair')
+    parser.add_argument('--class_freq')
 
     args = parser.parse_args()
 
-    save_data = label_count(args.train, args.meSH_pair_path)
-    with open('class_freq.pkl', 'wb') as f:
+    save_data = new_label_mapping(args.train, args.meSH_pair_path, args.new_meSH_pair)
+    with open(args.class_freq, 'wb') as f:
         pickle.dump(save_data, f, pickle.HIGHEST_PROTOCOL)
 
 
