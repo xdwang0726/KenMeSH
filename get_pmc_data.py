796de891d6a92244bc954872e2323da5ccabd83f
xdwang0726 <starwxd@gmail.com>
2021-09-13 12:29:10 +0800

    debug

--- get_pmc_data.py
+++ get_pmc_data.py
@@ -1,4 +1,6 @@
 import argparse
+import ijson
+import json
 import os
 import pickle
 import urllib.request
@@ -6,6 +8,18 @@ import xml.etree.ElementTree as ET
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+
+
+def from_mesh2id(labels_list, mapping_id):
+    mesh_id = []
+    for mesh in labels_list:
+        index = mapping_id.get(mesh.strip())
+        if index is None:
+            print(index)
+            pass
+        else:
+            mesh_id.append(index.strip())
+    return mesh_id
 
 
 def get_pmids_from_pmc(filelist):
@@ -96,7 +110,62 @@ def check_if_has_meshID(file):
         if medlines.find('MeshHeadingList') is None:
             pmids_no_mesh.append(pmid)
 
+    print('number of ids without mesh %d' % pmid)
+
     return pmids_no_mesh
+
+
+def get_data(pmid_path, mapping_path, allMesh):
+
+    pmids = []
+    with open(pmid_path, 'r') as f:
+        for ids in f:
+            pmids.append(ids.strip())
+
+    mapping_id = {}
+    with open(mapping_path) as f:
+        for line in f:
+            (key, value) = line.split('=')
+            mapping_id[key] = value
+
+    f = open(allMesh, encoding="utf8", errors='ignore')
+
+    objects = ijson.items(f, 'articles.item')
+
+    dataset = []
+    missed_id = []
+    for i, obj in enumerate(tqdm(objects)):
+        data_point = {}
+        ids = obj['pmid']
+        if ids in set(pmids):
+            try:
+                heading = obj['title'].strip()
+                heading = heading.translate(str.maketrans('', '', '[]'))
+                abstract = obj['abstractText'].strip()
+                abstract = abstract.translate(str.maketrans('', '', '[]'))
+                if len(heading) == 0 or heading == 'In process':
+                    print('paper ', ids, ' does not have title!')
+                    continue
+                elif len(abstract) == 0:
+                    print('paper ', ids, ' does not have abstract!')
+                    continue
+                else:
+                    label = obj["meshMajor"]
+                    journal = obj['journal']
+                    data_point['pmid'] = ids
+                    data_point['title'] = heading
+                    data_point['abstractText'] = abstract
+                    data_point['meshMajor'] = label
+                    data_point['meshId'] = from_mesh2id(label, mapping_id)
+                    data_point['journal'] = journal
+                    dataset.append(data_point)
+            except AttributeError:
+                print(obj["pmid"])
+        else:
+            missed_id.append(ids)
+            continue
+    pubmed = {'articles': dataset}
+    return pubmed, missed_id
 
 
 def main():
@@ -105,6 +174,11 @@ def main():
     parser.add_argument('--path')
     parser.add_argument('--pmids')
     parser.add_argument('--save')
+    parser.add_argument('--pmid_path')
+    parser.add_argument('--mapping_path')
+    parser.add_argument('--allMesh')
+    parser.add_argument('--save_dataset')
+    parser.add_argument('--save_missed')
 
     args = parser.parse_args()
 
@@ -124,12 +198,18 @@ def main():
 
     new_pmids = list(set(pmids) - set(no_mesh_pmid_list))
     print('Total number of articles %d' % len(new_pmids))
+    # #
+    # # pickle.dump(pmid_list, open(args.pmids, 'wb'))
     #
-    # pickle.dump(pmid_list, open(args.pmids, 'wb'))
-
     with open(args.save, 'w') as f:
         for ids in new_pmids:
             f.write('%s\n' % ids)
+
+    # pubmed, missed_ids = get_data(args.pmid_path, args.mapping_path, args.allMesh)
+    #
+    # with open(args.save_dataset, "w") as outfile:
+    #     json.dump(pubmed, outfile, indent=4)
+    # pickle.dump(missed_ids, open(args.save_missed, 'wb'))
 
 
 if __name__ == "__main__":
