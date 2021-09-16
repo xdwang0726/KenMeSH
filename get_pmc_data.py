dabe2aab98919b5317d1166b217bfebe45861904
xdwang0726 <starwxd@gmail.com>
2021-09-16 16:29:53 +0800

    merge json

--- get_pmc_data.py
+++ get_pmc_data.py
@@ -257,21 +257,21 @@ def main():
 
     args = parser.parse_args()
 
-    pmcs_list = []
-    with open(args.pmids, 'r') as f:
-        for ids in f:
-            pmcs_list.append(ids.strip())
-    print('mannually annoted articles: %d' % len(pmcs_list))
-
-    data = []
-    for root, dirs, files in os.walk(args.path):
-        for file in tqdm(files):
-            filename, extension = os.path.splitext(file)
-            if extension == '.xml':
-                dataset = get_data_from_xml(file, pmcs_list)
-                data.extend(dataset)
-    print('Total number of articles %d' % len(data))
-    pubmed = {'articles': data}
+    # pmcs_list = []
+    # with open(args.pmids, 'r') as f:
+    #     for ids in f:
+    #         pmcs_list.append(ids.strip())
+    # print('mannually annoted articles: %d' % len(pmcs_list))
+    #
+    # data = []
+    # for root, dirs, files in os.walk(args.path):
+    #     for file in tqdm(files):
+    #         filename, extension = os.path.splitext(file)
+    #         if extension == '.xml':
+    #             dataset = get_data_from_xml(file, pmcs_list)
+    #             data.extend(dataset)
+    # print('Total number of articles %d' % len(data))
+    # pubmed = {'articles': data}
     # no_mesh_pmid_list = list(set([ids for pmids in no_mesh for ids in pmids]))
     #
     # new_pmids = list(set(pmids_list) - set(no_mesh_pmid_list))
@@ -285,7 +285,7 @@ def main():
 
     # pubmed, missed_ids = get_data(args.pmid_path, args.mapping_path, args.allMesh)
     #
-    # pubmed = merge_json(args.path)
+    pubmed = merge_json(args.path)
     with open(args.save_dataset, "w") as outfile:
         json.dump(pubmed, outfile, indent=4)
 
