0d29b0aff50c8e5f231b48a6f96f02210cb7bb88
xdwang0726 <starwxd@gmail.com>
2021-09-28 17:30:38 +0800

    gat

--- run_classifier_multigcn.py
+++ run_classifier_multigcn.py
@@ -270,11 +270,11 @@ def train(train_dataset, valid_dataset, model, mlb, G, batch_sz, num_epochs, cri
             abstract_length = torch.Tensor(abstract_length)
             title_length = torch.Tensor(title_length)
             abstract, title, label, mask, abstract_length, title_length = abstract.to(device), title.to(device), label.to(device), mask.to(device), abstract_length.to(device), title_length.to(device)
-            # G = G.to(device)
+            G = G.to(device)
             G.ndata['feat'] = G.ndata['feat'].to(device)
             # G_c = G_c.to(device)
-            output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
-            # output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
+            # output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
+            output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
             # output = model(abstract, title, G.ndata['feat'])
 
             optimizer.zero_grad()
@@ -351,14 +351,14 @@ def test(test_dataset, model, mlb, G, batch_sz, device):
         abstract_length = torch.Tensor(abstract_length)
         title_length = torch.Tensor(title_length)
         mask, abstract, title, abstract_length, title_length = mask.to(device), abstract.to(device), title.to(device), abstract_length.to(device), title_length.to(device)
-        # G, G.ndata['feat'] = G.to(device), G.ndata['feat'].to(device)
-        G.ndata['feat'] = G.ndata['feat'].to(device)
+        G, G.ndata['feat'] = G.to(device), G.ndata['feat'].to(device)
+        # G.ndata['feat'] = G.ndata['feat'].to(device)
         # G_c, G_c.ndata['feat'] = G_c.to(device), G_c.ndata['feat'].to(device)
         label = mlb.fit_transform(label)
 
         with torch.no_grad():
-            output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
-            # output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat'])
+            # output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
+            output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat'])
             # output = model(abstract, title, G.ndata['feat'])
             # pred = torch.cat((pred, output), dim=0)
 
@@ -512,11 +512,11 @@ def main():
                         args.word2vec_path, args.graph, args.num_example) # args. graph_cooccurence,
     # neg_pos_ratio = pickle.load(open(args.neg_pos, 'rb'))
     vocab_size = len(vocab)
-    # model = multichannel_dilatedCNN_with_MeSH_mask(vocab_size, args.dropout, args.ksz, num_nodes, G, device,
-    #                                                embedding_dim=200, rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
+    model = multichannel_dilatedCNN_with_MeSH_mask(vocab_size, args.dropout, args.ksz, num_nodes, G, device,
+                                                   embedding_dim=200, rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
                                     #gat_num_heads=8, gat_num_layers=2, gat_num_out_heads=1)
-    model = multichannel_dilatedCNN_without_graph(vocab_size, args.dropout, args.ksz, num_nodes, embedding_dim=200,
-                                                  rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
+    # model = multichannel_dilatedCNN_without_graph(vocab_size, args.dropout, args.ksz, num_nodes, embedding_dim=200,
+    #                                               rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
     model.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors)).to(device)
 
     model.to(device)
