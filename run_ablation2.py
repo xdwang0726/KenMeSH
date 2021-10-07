f0c15b2fa7513aca296c7bc962d3b3c3b0911e34
xdwang0726 <starwxd@gmail.com>
2021-10-07 18:12:51 +0800

    ablation test

--- run_ablation2.py
+++ run_ablation2.py
@@ -108,7 +108,6 @@ def prepare_dataset(title_path, abstract_path, label_path, mask_path, MeSH_id_pa
     # test_text = all_text[-20000:]
     # test_label_id = label_id[-20000:]
     # test_mesh_mask = mesh_mask[-20000:]
-    # train_label = label_id[:num_example]
 
     # for i, obj in enumerate(tqdm(test_objects)):
     #     if 13000 < i <= 14000:
@@ -275,7 +274,7 @@ def train(train_dataset, valid_dataset, model, mlb, G, batch_sz, num_epochs, cri
             abstract_length = torch.Tensor(abstract_length)
             title_length = torch.Tensor(title_length)
             abstract, title, label, mask, abstract_length, title_length = abstract.to(device), title.to(device), label.to(device), mask.to(device), abstract_length.to(device), title_length.to(device)
-            # G = G.to(device)
+            G = G.to(device)
             G.ndata['feat'] = G.ndata['feat'].to(device)
             output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
             # output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
@@ -307,11 +306,11 @@ def train(train_dataset, valid_dataset, model, mlb, G, batch_sz, num_epochs, cri
                 abstract_length = torch.Tensor(abstract_length)
                 title_length = torch.Tensor(title_length)
                 abstract, title, label, mask, abstract_length, title_length = abstract.to(device), title.to(device), label.to(device), mask.to(device), abstract_length.to(device), title_length.to(device)
-                # G = G.to(device)
+                G = G.to(device)
                 G.ndata['feat'] = G.ndata['feat'].to(device)
 
-                # output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
-                output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
+                output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
+                # output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
 
                 loss = criterion(output, label)
                 valid_losses.append(loss.item())
@@ -361,12 +360,12 @@ def test(test_dataset, model, mlb, G, batch_sz, device):
             abstract_length = torch.Tensor(abstract_length)
             title_length = torch.Tensor(title_length)
             mask, abstract, title, abstract_length, title_length = mask.to(device), abstract.to(device), title.to(device), abstract_length.to(device), title_length.to(device)
-            # G, G.ndata['feat'] = G.to(device), G.ndata['feat'].to(device)
-            G.ndata['feat'] = G.ndata['feat'].to(device)
+            G, G.ndata['feat'] = G.to(device), G.ndata['feat'].to(device)
+            # G.ndata['feat'] = G.ndata['feat'].to(device)
             label = mlb.fit_transform(label)
-            # output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat']) #, G_c, G_c.ndata['feat'])
-            # output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat'])
-            output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
+
+            output = model(abstract, title, mask, abstract_length, title_length, G, G.ndata['feat'])
+            # output = model(abstract, title, mask, abstract_length, title_length, G.ndata['feat'])
             # output = model(abstract, title, G.ndata['feat'])
             # pred = torch.cat((pred, output), dim=0)
 
@@ -556,7 +555,7 @@ def main():
     # model = multichannel_dilatedCNN(vocab_size, args.dropout, args.ksz, num_nodes, G, device, embedding_dim=200,
     #                                 rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
 
-    model = multichannel_dilatedCNN_without_graph(vocab_size, args.dropout, args.ksz, num_nodes, embedding_dim=200,
+    model = multichannel_with_MeSH_mask(vocab_size, args.dropout, args.ksz, num_nodes, G, device, embedding_dim=200,
                                                   rnn_num_layers=2, cornet_dim=1000, n_cornet_blocks=2)
     model.embedding_layer.weight.data.copy_(weight_matrix(vocab, vectors)).to(device)
 
