815d97e879733e4dc62b188014fc93fae7fd0f8a
xdwang0726 <starwxd@gmail.com>
2021-10-13 19:53:35 +0800

    ablation3

--- run_classifier_multigcn.py
+++ run_classifier_multigcn.py
@@ -153,8 +153,8 @@ def prepare_dataset(title_path, abstract_path, label_path, mask_path, MeSH_id_pa
 
     # Preparing training and test datasets
     print('prepare training and test sets')
-    dataset = MeSH_indexing(all_text, all_title, all_text[400000:num_example], all_title[400000:num_example], label_id[400000:num_example],
-                            mesh_mask[400000:num_example], all_text[-20000:], all_title[-20000:], label_id[-20000:],
+    dataset = MeSH_indexing(all_text, all_title, all_text[:num_example], all_title[:num_example], label_id[:num_example],
+                            mesh_mask[:num_example], all_text[-20000:], all_title[-20000:], label_id[-20000:],
                             mesh_mask[-20000:], is_test=False, is_multichannel=True)
 
     # build vocab
@@ -164,8 +164,8 @@ def prepare_dataset(title_path, abstract_path, label_path, mask_path, MeSH_id_pa
     # get validation set
     valid_size = 0.02
     # indices = list(range(len(pmid)))
-    split = int(np.floor(valid_size * len(all_title[400000:num_example])))
-    train_dataset, valid_dataset = random_split(dataset=dataset, lengths=[len(all_title[400000:num_example]) - split, split])
+    split = int(np.floor(valid_size * len(all_title[:num_example])))
+    train_dataset, valid_dataset = random_split(dataset=dataset, lengths=[len(all_title[:num_example]) - split, split])
     # train_idx, valid_idx = indices[split:], indices[:split]
     # train_sampler = SubsetRandomSampler(train_idx)
     # valid_sampler = SubsetRandomSampler(valid_idx)
@@ -569,10 +569,10 @@ def main():
     # pre-allocate GPU memory
     # preallocate_gpu_memory(G, model, args.batch_sz, device, num_nodes, criterion)
 
-    # load model
-    model.load_state_dict(torch.load(args.model))
-    model.to(device)
-    model.train()
+    # # load model
+    # model.load_state_dict(torch.load(args.model))
+    # model.to(device)
+    # model.train()
     # training
     print("Start training!")
     model, train_loss, valid_loss = train(train_dataset, valid_dataset, model, mlb, G, args.batch_sz,
